e4c36691065bf95227fead2a1fad7e3e3254ffd0
KilianCallebaut <kilian.callebaut@gmail.com [core]>
2017-11-12 23:19:45 +0100

    Updated files

---
+++ Assets/Scripts/AgentAttributes.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentAttributes {
+
+
+    public float speed { get;  private set; }
+    public float reachOfVision { get; private set; }
+    public float widthOfVision { get;  private set;}
+    public float reflex { get; private set; }
+    public float agility  { get;  private set;}
+    public float accuracy  { get;  private set;}
+
+    // Personalized agentattributes
+    public AgentAttributes(float speed, float reachOfVision, float widthOfVision,
+        float reflex, float agility, float accuracy)
+    {
+        this.speed = speed;
+        this.reachOfVision = reachOfVision;
+        this.widthOfVision = widthOfVision;
+        this.reflex = reflex;
+        this.agility = agility;
+        this.accuracy = accuracy;
+
+    }
+
+    // Default/debugger agentattributes
+    public AgentAttributes()
+    {
+        this.speed = 2.0f;
+        this.reachOfVision = 5.0f;
+        this.widthOfVision = 50.0f;
+        this.reflex = 1.0f / 2.0f;
+        this.agility = 2.0f;
+        this.accuracy = 10.0f;
+    }
+
+}
---
+++ Assets/Scripts/ObjectManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager : MonoBehaviour {
+
+ 
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public static GameObject spawnAgent(GameObject agent, AgentAttributes attributes)
+    {
+        var agentSpawn = Instantiate(agent);
+        Agent agentScript = agentSpawn.AddComponent<Agent>() as Agent;
+     
+        agentScript.Attributes = attributes;
+        return agentSpawn;
+    }
+}
