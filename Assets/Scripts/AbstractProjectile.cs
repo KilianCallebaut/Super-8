b0c01e7b9dd2baa714db28a0274fbb6b1d3f3ca7
Simon Ragnesand <binamrad@gmail.com>
2017-11-13 00:24:27 +0100

    Initial weapon and abstract projectile scripts
    What it says on the tin. Still needs some work.

---
+++ Assets/Scripts/AbstractProjectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractProjectile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	protected Vector3 dir;
+	protected float vel;
+	protected GameObject ignoreCollisionsWith = null;
+
+
+	public void initialUpdate(float dTime, GameObject doNotCollideWith, Vector3 direction) {
+		dir.x = direction.x;
+		dir.y = direction.y;
+		ignoreCollisionsWith = doNotCollideWith;
+		transform.Translate (dir * vel * dTime);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (dir * vel * UnityEngine.Time.deltaTime);
+	}
+}
