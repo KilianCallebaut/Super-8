d1f95715b558496d456eb021c3e104e61a4cb5a9
Simon Ragnesand <binamrad@gmail.com>
2017-11-23 01:07:01 +0100

    did stuff, hopefully this fixes shit

---
+++ Assets/Scripts/CapturePointScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	void OnTrigger2DEnter() {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
