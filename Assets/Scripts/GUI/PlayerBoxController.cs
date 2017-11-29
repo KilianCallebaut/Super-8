39f0452d3dda2e3b07f5cf202fb3b5a4b086747a
galnegus <tobiashindersson@hotmail.com>
2017-11-29 22:51:20 +0100

    Add basic screens with transitions and some player boxes

---
+++ Assets/Scripts/GUI/PlayerBoxController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerBoxController : MonoBehaviour {
+
+    private AgentAttributes _Attributes;
+    public AgentAttributes Attributes {
+        private get { return _Attributes; }
+        set
+        {
+            _Attributes = value;
+            Name.text = "Temp Name";
+            SpeedValue.text = value.speed.ToString("0.0");
+            ReachOfVisionValue.text = value.reachOfVision.ToString("0.0");
+            WidthOfVisionValue.text = value.widthOfVision.ToString("0.0");
+            ReflexValue.text = value.reflex.ToString("0.0");
+            AgilityValue.text = value.agility.ToString("0.0");
+            AccuracyValue.text = value.accuracy.ToString("0.0");
+        }
+    }
+
+    public Text Name;
+    public Text SpeedValue;
+    public Text ReachOfVisionValue;
+    public Text WidthOfVisionValue;
+    public Text ReflexValue;
+    public Text AgilityValue;
+    public Text AccuracyValue;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/TeamManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamManager : MonoBehaviour {
+
+    public GameObject PlayersScreen;
+    public GameObject PlayerBoxPrefab;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 8; ++i)
+        {
+            var playerBox = Instantiate(PlayerBoxPrefab);
+            playerBox.transform.parent = PlayersScreen.transform;
+            PlayerBoxController controller = playerBox.gameObject.GetComponent<PlayerBoxController>();
+            controller.Attributes = new AgentAttributes(null);
+            //agents.Add(new AgentAttributes(null));
+        }
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
